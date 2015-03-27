e1a56d02e0dd5741d11d7bfd6cd4bd00aad155ca
Theodore DeRego <tderego94@gmail.com>
2015-03-27 13:47:21 -1000

    Updated to latest Rust and Piston. Fixes #1

--- sample_project/src/main.rs
+++ sample_project/src/main.rs
@@ -1,30 +1,40 @@
 extern crate piston;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate opengl_graphics;
+
+use std::cell::RefCell;
+use opengl_graphics::{
+    GlGraphics,
+    OpenGL,
+};
+use sdl2_window::Sdl2Window;
 
 fn main() {
-  piston::start(
-    piston::shader_version::OpenGL::_3_2,
-    piston::WindowSettings {
-      title: "Hello Piston".to_string(),
-      samples: 0,
-      size: [300, 300],
-      fullscreen: false,
-      exit_on_esc: true
-    },
-    || start()
-  );
-}
+    let opengl = OpenGL::_3_2;
+    let window = Sdl2Window::new(
+        opengl,
+        piston::window::WindowSettings {
+            title: "Hello Piston".to_string(),
+            size: [300, 300],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 0,
+        }
+    );
 
-fn start() {
-  for e in piston::events() {
-    use piston::event::RenderEvent;
-    use piston::graphics::Rectangle;
+    let ref mut gl = GlGraphics::new(opengl);
+    let window = RefCell::new(window);
+    for e in piston::events(&window) {
+        use piston::event::*;
 
-    e.render(|_| {
-      piston::render_2d_opengl(
-        Some(piston::graphics::color::WHITE), |c, g| {
-          Rectangle::new([1.0, 0.0, 0.0, 1.0])
-            .draw([0.0, 0.0, 100.0, 100.0], &c, g);
-      });
-    });
-  }
+        if let Some(args) = e.render_args() {
+            use graphics::*;
+
+            gl.draw([0, 0, args.width as i32, args.height as i32], |c, gl| {
+                clear([1.0; 4], gl);
+                rectangle([1.0, 0.0, 0.0, 1.0], [0.0, 0.0, 100.0, 100.0], c.transform, gl);
+            });
+        };
+    }
 }
